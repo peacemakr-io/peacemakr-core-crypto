0855701ef3c4c03d61d867e473488bf02271080d
aman <aman.lachapelle@gmail.com>
2018-08-18 13:24:08 -0700

    Add go bindings
    * Update go tests to be more comprehensive

--- src/ffi/go/src/peacemakr_core_crypto/crypto_test.go
+++ src/ffi/go/src/peacemakr_core_crypto/crypto_test.go
@@ -5,75 +5,120 @@ import (
 	"testing"
 )
 
-func TestEncrypt(t *testing.T) {
-	cfg := CryptoConfig{
-		mode:             ASYMMETRIC,
-		asymmetricCipher: RSA_2048,
-		symmetricCipher:  CHACHA20_POLY1305,
-		digestAlgorithm:  SHA_384,
+func TestAsymmetricEncrypt(t *testing.T) {
+	for i := RSA_2048; i <= RSA_4096; i++ {
+		for j := AES_128_GCM; j <= CHACHA20_POLY1305; j++ {
+			cfg := CryptoConfig{
+				mode:             ASYMMETRIC,
+				asymmetricCipher: i,
+				symmetricCipher:  j,
+				digestAlgorithm:  SHA_512,
+			}
+
+			plaintextIn := Plaintext{
+				data: []byte("Hello world, I'm testing encryption from go!"),
+				aad:  []byte("And I'm AAD from go"),
+			}
+
+			randomDevice := NewRandomDevice()
+
+			key := NewPeacemakrKey(cfg, randomDevice)
+
+			ciphertext := Encrypt(cfg, key, plaintextIn, randomDevice)
+
+			plaintextOut, success := Decrypt(key, ciphertext)
+			if !success {
+				t.Fatalf("Decrypt failed")
+			}
+
+			if !bytes.Equal(plaintextIn.data, plaintextOut.data) {
+				t.Fatalf("plaintext data did not match")
+			}
+
+			if !bytes.Equal(plaintextIn.aad, plaintextOut.aad) {
+				t.Fatalf("plaintext data did not match")
+			}
+		}
 	}
+}
 
-	plaintextIn := Plaintext{
-		data: []byte("Hello world, I'm testing encryption from go!"),
-		aad:  []byte("And I'm AAD from go"),
-	}
+func TestSymmetricEncrypt(t *testing.T) {
+	for j := AES_128_GCM; j <= CHACHA20_POLY1305; j++ {
+		cfg := CryptoConfig{
+			mode:             SYMMETRIC,
+			asymmetricCipher: NONE,
+			symmetricCipher:  j,
+			digestAlgorithm:  SHA_512,
+		}
 
-	randomDevice := NewRandomDevice()
+		plaintextIn := Plaintext{
+			data: []byte("Hello world, I'm testing encryption from go!"),
+			aad:  []byte("And I'm AAD from go"),
+		}
 
-	key := NewPeacemakrKey(cfg, randomDevice)
+		randomDevice := NewRandomDevice()
 
-	ciphertext := Encrypt(cfg, key, plaintextIn, randomDevice)
+		key := NewPeacemakrKey(cfg, randomDevice)
 
-	plaintextOut, success := Decrypt(key, ciphertext)
-	if !success {
-		t.Fatalf("Decrypt failed")
-	}
+		ciphertext := Encrypt(cfg, key, plaintextIn, randomDevice)
 
-	if !bytes.Equal(plaintextIn.data, plaintextOut.data) {
-		t.Fatalf("plaintext data did not match")
-	}
+		plaintextOut, success := Decrypt(key, ciphertext)
+		if !success {
+			t.Fatalf("Decrypt failed")
+		}
 
-	if !bytes.Equal(plaintextIn.aad, plaintextOut.aad) {
-		t.Fatalf("plaintext data did not match")
+		if !bytes.Equal(plaintextIn.data, plaintextOut.data) {
+			t.Fatalf("plaintext data did not match")
+		}
+
+		if !bytes.Equal(plaintextIn.aad, plaintextOut.aad) {
+			t.Fatalf("plaintext data did not match")
+		}
 	}
 }
 
 func TestSerialize(t *testing.T) {
-	cfg := CryptoConfig{
-		mode:             ASYMMETRIC,
-		asymmetricCipher: RSA_2048,
-		symmetricCipher:  CHACHA20_POLY1305,
-		digestAlgorithm:  SHA_384,
-	}
+	for i := RSA_2048; i <= RSA_4096; i++ {
+		for j := AES_128_GCM; j <= CHACHA20_POLY1305; j++ {
+			for k := SHA_224; k <= SHA_512; k++ {
+				cfg := CryptoConfig{
+					mode:             ASYMMETRIC,
+					asymmetricCipher: i,
+					symmetricCipher:  j,
+					digestAlgorithm:  k,
+				}
 
-	plaintextIn := Plaintext{
-		data: []byte("Hello world, I'm testing encryption from go!"),
-		aad:  []byte("And I'm AAD from go"),
-	}
+				plaintextIn := Plaintext{
+					data: []byte("Hello world, I'm testing encryption from go!"),
+					aad:  []byte("And I'm AAD from go"),
+				}
 
-	randomDevice := NewRandomDevice()
+				randomDevice := NewRandomDevice()
 
-	key := NewPeacemakrKey(cfg, randomDevice)
+				key := NewPeacemakrKey(cfg, randomDevice)
 
-	ciphertext := Encrypt(cfg, key, plaintextIn, randomDevice)
+				ciphertext := Encrypt(cfg, key, plaintextIn, randomDevice)
 
-	serialized := Serialize(ciphertext)
-	if serialized == nil {
-		t.Fatalf("serialize failed")
-	}
+				serialized := Serialize(ciphertext)
+				if serialized == nil {
+					t.Fatalf("serialize failed")
+				}
 
-	newCiphertext := Deserialize(serialized)
+				newCiphertext := Deserialize(serialized)
 
-	plaintextOut, success := Decrypt(key, newCiphertext)
-	if !success {
-		t.Fatalf("Decrypt failed")
-	}
+				plaintextOut, success := Decrypt(key, newCiphertext)
+				if !success {
+					t.Fatalf("Decrypt failed")
+				}
 
-	if !bytes.Equal(plaintextIn.data, plaintextOut.data) {
-		t.Fatalf("plaintext data did not match")
-	}
+				if !bytes.Equal(plaintextIn.data, plaintextOut.data) {
+					t.Fatalf("plaintext data did not match")
+				}
 
-	if !bytes.Equal(plaintextIn.aad, plaintextOut.aad) {
-		t.Fatalf("plaintext data did not match")
+				if !bytes.Equal(plaintextIn.aad, plaintextOut.aad) {
+					t.Fatalf("plaintext data did not match")
+				}
+			}
+		}
 	}
 }
