824f2579a7cfb6bfcd543603da0681a42a774234
aman <aman.lachapelle@gmail.com>
2018-08-18 22:31:51 -0700

    Add go bindings
    * add license header

--- src/ffi/go/src/peacemakr_core_crypto/crypto_test.go
+++ src/ffi/go/src/peacemakr_core_crypto/crypto_test.go
@@ -1,3 +1,11 @@
+//
+// Created by Aman LaChapelle on 7/20/18.
+//
+// peacemakr-core-crypto
+// Copyright (c) 2018 peacemakr
+// Full license at peacemakr-core-crypto/LICENSE.txt
+//
+
 package peacemakr_core_crypto
 
 import (
