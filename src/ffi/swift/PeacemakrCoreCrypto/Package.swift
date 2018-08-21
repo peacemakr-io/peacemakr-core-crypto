0834fcc07b2ce0ea5d609b5f992d554dd938aa04
aman <aman.lachapelle@gmail.com>
2018-08-20 18:45:51 -0700

    Squashed commit of the following:
    commit 51af1a38210bcb500a3cc52f4954e732cd5467ee
    Author: aman <aman.lachapelle@gmail.com>
    Date:   Mon Aug 20 18:09:31 2018 -0700
    
        Add swift bindings
    
        * Add tests, remove dockerfile since we will be building on macOS anyway (and getting the Security framework was going to be a huge pain)
    
    commit b4d6ffe1f5e6f66353d18af8718bc1c6064df8db
    Author: aman <aman.lachapelle@gmail.com>
    Date:   Sun Aug 19 18:41:59 2018 -0700
    
        Add swift bindings
    
        * attempt to bind the code in crypto.h to swift, no tests yet
    
    commit 9bc63eb186b0fdf1cb58f01eb98970d33d961e94
    Author: aman <aman.lachapelle@gmail.com>
    Date:   Sat Aug 18 22:43:16 2018 -0700
    
        Add go bindings
    
        * add pseudo package to make using the swift code easier
    
    commit b08bb7cbff1e7b3df7302a6e72ce1a5b0f6f86e1
    Author: aman <aman.lachapelle@gmail.com>
    Date:   Sat Aug 18 22:31:16 2018 -0700
    
        Add swift bindings
    
        * add swift package
    
    commit a0c2f190d8868f39210a7176823cfb5884236594
    Author: aman <aman.lachapelle@gmail.com>
    Date:   Sat Aug 18 14:41:44 2018 -0700
    
        Add swift bindings
    
        * clang-format
    
    commit 9097651cf87425c71fcf256621cadf61269fad74
    Author: aman <aman.lachapelle@gmail.com>
    Date:   Sat Aug 18 13:49:12 2018 -0700
    
        Add swift bindings
    
        * debian has the recent version of openssl
    
    commit 9e76db7c31cfd87605d2c8d8400325ff1b34d6c6
    Author: aman <aman.lachapelle@gmail.com>
    Date:   Sat Aug 18 13:30:38 2018 -0700
    
        Add swift bindings
    
        * Add swift dockerfile

---
+++ src/ffi/swift/PeacemakrCoreCrypto/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PeacemakrCoreCrypto",
+    products: [
+        .library(
+            name: "PeacemakrCoreCrypto",
+            targets: ["PeacemakrCoreCrypto"]),
+    ],
+    targets: [
+        .target(
+            name: "PeacemakrCoreCrypto",
+            dependencies: []),
+        .testTarget(
+            name: "PeacemakrCoreCryptoTests",
+            dependencies: ["PeacemakrCoreCrypto"]),
+    ]
+)
