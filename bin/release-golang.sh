de1216f909155b61f4a63aac551ee6932b81228b
Jon McLachlan <jon.mclachlan@icloud.com>
2018-09-01 11:54:47 -0700

    And publish and tag and all that good stuff (#19)

--- bin/release-golang.sh
+++ bin/release-golang.sh
@@ -3,11 +3,11 @@
 set -ex
 
 function usage {
-    echo "Usage: ./bin/release-golang.sh [path to include folder] [path to go root]"
-    echo "for example, ./bin/release-golang.sh ../peacemakr-api/include/ ../peacemakr-api/"
+    echo "Usage: ./bin/release-golang.sh [path to include folder] [path to go root] [version]"
+    echo "for example, ./bin/release-golang.sh ../peacemakr-api/include/ ../peacemakr-api/ 0.0.4"
 }
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 3 ]; then
     echo "Illegal use"
     usage
     exit 1
@@ -17,9 +17,10 @@ function get_crypto_file {
 
     h_dir=${1}
     go_root=${2}
+    version=${3}
 
     docker rm src-container || true
-    IMG_ID=$(docker run --name src-container -d go-builder sh)
+    IMG_ID=$(docker run --name src-container -d peacemkar-core-crypto:${version} sh)
     docker cp src-container:/go/src/peacemakr/crypto/crypto.go ${go_root}/src/peacemakr/crypto/crypto.go
     docker cp src-container:/go/src/peacemakr/crypto/crypto_test.go ${go_root}/src/peacemakr/crypto/crypto_test.go
     docker cp src-container:/usr/local/include/peacemakr/crypto.h ${h_dir}/peacemakr/crypto.h
@@ -27,5 +28,9 @@ function get_crypto_file {
     docker kill ${IMG_ID} || true
 }
 
-docker build -t go-builder . -f docker/go.Dockerfile
-get_crypto_file ${1} ${2}
+export AWS_PROFILE=peacemakr
+aws ecr get-login --no-include-email | sh
+docker build -t peacemkar-core-crypto:${3} . -f docker/go.Dockerfile
+docker tag peacemkar-core-crypto:${3} 716293438869.dkr.ecr.us-east-2.amazonaws.com/peacemakr-core-crypto:${3}
+docker push 716293438869.dkr.ecr.us-east-2.amazonaws.com/peacemakr-core-crypto:${3}
+get_crypto_file ${1} ${2} ${3}
