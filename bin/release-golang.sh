67ea5162bf482d4f1efebbdf065cfad085379d29
Jon McLachlan <jon.mclachlan@icloud.com>
2018-09-01 11:06:52 -0700

    Release alllll the things

--- bin/release-golang.sh
+++ bin/release-golang.sh
@@ -1,33 +1,31 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 function usage {
-    echo "Usage: ./release-golang.sh OPTIONS"
-    echo "Options: "
-    echo "  -o <path-to-file-output>"
+    echo "Usage: ./bin/release-golang.sh [path to include folder] [path to go root]"
+    echo "for example, ./bin/release-golang.sh ../peacemakr-api/include/ ../peacemakr-api/"
 }
+
+if [ "$#" -ne 2 ]; then
+    echo "Illegal use"
+    usage
+    exit 1
+fi
 
 function get_crypto_file {
 
-    FILE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+    h_dir=${1}
+    go_root=${2}
 
-    output_dir=${FILE_DIR}/../src/ffi/go/src/peacemakr/crypto/
-
-    while getopts "o:h" o; do
-    case "${o}" in
-        o) output_dir=${OPTARG}
-           ;;
-        h) usage
-        esac
-    done
-    shift $((OPTIND-1))
-
-    pushd ${FILE_DIR}/..
-    docker build -t go-builder . -f docker/go.Dockerfile
+    docker rm src-container || true
     IMG_ID=$(docker run --name src-container -d go-builder sh)
-    docker cp src-container:/go/src/peacemakr/crypto/crypto.go ${output_dir}
-    docker kill ${IMG_ID}
+    docker cp src-container:/go/src/peacemakr/crypto/crypto.go ${go_root}/src/peacemakr/crypto/crypto.go
+    docker cp src-container:/go/src/peacemakr/crypto/crypto_test.go ${go_root}/src/peacemakr/crypto/crypto_test.go
+    docker cp src-container:/usr/local/include/peacemakr/crypto.h ${h_dir}/peacemakr/crypto.h
+    docker cp src-container:/usr/local/include/peacemakr/random.h ${h_dir}/peacemakr/random.h
+    docker kill ${IMG_ID} || true
 }
 
-get_crypto_file "$@"
+docker build -t go-builder . -f docker/go.Dockerfile
+get_crypto_file ${1} ${2}
