6875f6a7a13c62d81f5180ee3f6d2b8c6624a88d
aman <aman.lachapelle@gmail.com>
2018-11-03 12:10:15 -0700

    Automate iOS build to just `./release-ios.sh`

--- bin/release-ios.sh
+++ bin/release-ios.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
-mkdir -p build && cd build
-cmake .. -DPEACEMAKR_BUILD_IOS=ON
+set -ex
+
+pushd ..
+mkdir -p ios-build
+pushd ios-build
+cmake .. -DPEACEMAKR_BUILD_CPP=OFF -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 -DOPENSSL_LIBRARIES=/usr/local/opt/openssl@1.1/lib -DPEACEMAKR_BUILD_IOS=ON -DCMAKE_INSTALL_PREFIX=../src/ffi/swift/libCoreCrypto
+make install
+popd
+pushd src/ffi/swift/libCoreCrypto
+sh build-core-crypto.sh
+popd
+popd
