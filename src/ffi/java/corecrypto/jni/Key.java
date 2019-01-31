23ea58fb4b847273c3514a555280c199ae2c9cd4
bzcheeseman <alachapelle@uchicago.edu>
2019-01-30 18:01:36 -0800

    attempt to add jni stuff

--- src/ffi/java/corecrypto/jni/Key.java
+++ src/ffi/java/corecrypto/jni/Key.java
@@ -1,5 +1,16 @@
-package io.peacemakr.corecrypto.jni
+package io.peacemakr.corecrypto.jni;
 
 public class Key {
+    static {
+        System.loadLibrary("peacemakr-core-crypto");
+    }
+
+    private long nativeKey;
+
     // TODO
+//    private native PeacemakrKey_new()
+
+    public Key(CryptoConfig cfg, RandomDevice rand) {
+        ;
+    }
 }
