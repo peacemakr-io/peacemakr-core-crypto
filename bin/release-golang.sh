69ba01b01e14827620db792e5d3025070d0972eb
Aman LaChapelle <aman.lachapelle@gmail.com>
2019-04-13 09:06:28 -0700

    Fix golang release script (#74)

--- bin/release-golang.sh
+++ bin/release-golang.sh
@@ -17,8 +17,8 @@ function get_crypto_file {
 
     out_dir=${1}
 
-    docker run corecrypto:latest cat /go/src/peacemakr/crypto/crypto.go > ${out_dir}/src/peacemakr/crypto/crypto.go
-    docker run corecrypto:latest cat /go/src/peacemakr/crypto/crypto_test.go > ${out_dir}/src/peacemakr/crypto/crypto_test.go
+    docker run corecrypto:latest cat /go/src/peacemakr/crypto/crypto.go > ${out_dir}/crypto/crypto.go
+    docker run corecrypto:latest cat /go/src/peacemakr/crypto/crypto_test.go > ${out_dir}/crypto/crypto_test.go
     docker run corecrypto:latest cat /usr/local/include/peacemakr/crypto.h > ${out_dir}/include/peacemakr/crypto.h
     docker run corecrypto:latest cat /usr/local/include/peacemakr/random.h > ${out_dir}/include/peacemakr/random.h
 
@@ -33,14 +33,4 @@ fi
 docker build -t corecrypto-dependencies:latest . -f docker/go-dependencies.Dockerfile --build-arg=${BUILD_ARG}
 docker build -t corecrypto:latest . -f docker/go.Dockerfile --build-arg=${BUILD_ARG}
 get_crypto_file ${1}
-
-# Setup aws credentials
-`aws ecr get-login --no-include-email --region us-east-2 --profile peacemakr` || true
-
-# Release to ECR
-docker tag corecrypto-dependencies:latest 716293438869.dkr.ecr.us-east-2.amazonaws.com/corecrypto-dependencies:latest
-docker push 716293438869.dkr.ecr.us-east-2.amazonaws.com/corecrypto-dependencies:latest
-
-docker tag corecrypto:latest 716293438869.dkr.ecr.us-east-2.amazonaws.com/corecrypto:latest
-docker push 716293438869.dkr.ecr.us-east-2.amazonaws.com/corecrypto:latest
 
