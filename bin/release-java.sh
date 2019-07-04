e7ef7379f634499b56341e055d6c1eb3b4e325a4
bzcheeseman <alachapelle@uchicago.edu>
2019-07-03 22:59:54 -0700

    - android -> java

--- bin/release-java.sh
+++ bin/release-java.sh
@@ -3,7 +3,7 @@
 set -ex
 
 function usage {
-    echo "Usage: ANDROID_NDK_ROOT=/path/to/ndk-bundle ./release-android.sh [path to peacemakr-ios folder] [optional: first]"
+    echo "Usage: ANDROID_NDK_ROOT=/path/to/ndk-bundle ./release-java.sh [path to peacemakr-ios folder] [optional: first]"
 }
 
 if [[ "$#" -gt 2 ]]; then
@@ -25,7 +25,7 @@ pushd ..
 PROJECT_SRC=$(pwd)
 
 if [[ ! -z "${2}" ]]; then
-    pushd src/ffi/android/openssl
+    pushd src/ffi/java/openssl
     ./build-openssl.sh
     popd
 fi
