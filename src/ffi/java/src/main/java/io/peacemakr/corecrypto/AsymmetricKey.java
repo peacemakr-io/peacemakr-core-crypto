24e0b6320f8f1de183c6214ae13412ba994ef428
Jon McLachlan <jon.mclachlan@icloud.com>
2019-07-12 22:28:55 -0700

    That's sort of the interface we need for Java

---
+++ src/ffi/java/src/main/java/io/peacemakr/corecrypto/AsymmetricKey.java
@@ -0,0 +1,10 @@
+
+public class AsymmetricKey {
+
+	long asymmetricKey;
+
+	public AsymmetricKey() {
+		
+	}
+
+}
---
+++ src/ffi/java/src/main/java/io/peacemakr/corecrypto/Crypto.java
@@ -0,0 +1,24 @@
+package io.peacemakr.corecrypto;
+
+public class Crypto {
+    static {
+        System.loadLibrary("peacemakr-core-crypto-jni");
+    }
+
+    enum SymmetricCryptoTypes {
+        AES_GCM_128 = 0,
+        AES_GSM_192 = 1,
+        AES_GCM_256 = 2,
+        CHACHA20_POLY1902 = 3
+    }
+
+    public native static byte[] encryptSymmetric(byte[] key, byte[] keyId, AsymmetricKey signingKey, byte[] signingKeyId, byte[] plaintext, SymmetricCryptoTypes mode);
+
+    public native static byte[] getEncryptingKeyIdFromCiphertext(byte[] ciphertext);
+
+    public native static byte[] getSigningKeyIdFromCiphertext(byte[] ciphertext);
+
+    public native static byte[] decryptSymmetric(byte[] key, AsymmetricKey verificationKey, byte[] ciphertext);
+
+    public native static byte[] decryptAsymmetric(AsymmetricKey key, byte[] ciphertext);
+}
