6d2a92e5eae5f31f75676762e1b4470875098d9d
bzcheeseman <alachapelle@uchicago.edu>
2019-07-25 11:58:30 -0700

    Improvements
    - CI can tag and push images off git tags now
    - Add regressions list so we have a place to put repro tests

--- src/core/test/encrypt_symmetric.c
+++ src/core/test/encrypt_symmetric.c
@@ -178,58 +178,6 @@ void test_master_key_symmetric_algo(peacemakr_key_t *master_key,
   peacemakr_key_free(key);
 }
 
-void test_uninit_crash() {
-  crypto_config_t cfg = {.mode = SYMMETRIC,
-                         .symm_cipher = AES_128_GCM,
-                         .asymm_cipher = ASYMMETRIC_UNSPECIFIED,
-                         .digest_algorithm = SHA_256};
-
-  plaintext_t plaintext_in = {.data = (const unsigned char *)"Hello world!",
-                              .data_len = 12 + 1,
-                              .aad = NULL,
-                              .aad_len = 0};
-
-  plaintext_t plaintext_out;
-
-  random_device_t rand = get_default_random_device();
-
-  peacemakr_key_t *key = peacemakr_key_new_symmetric(AES_128_GCM, &rand);
-
-  ciphertext_blob_t *ciphertext = peacemakr_encrypt(key, &plaintext_in, &rand);
-  assert(ciphertext != NULL);
-
-  size_t out_size = 0;
-  uint8_t *serialized = peacemakr_serialize(SHA_256, ciphertext, &out_size);
-  assert(serialized != NULL);
-
-  crypto_config_t out_cfg;
-
-  ciphertext_blob_t *deserialized =
-      peacemakr_deserialize(serialized, out_size, &out_cfg);
-
-  free(serialized);
-
-  decrypt_code success = peacemakr_decrypt(key, deserialized, &plaintext_out);
-
-  assert((out_cfg.mode == cfg.mode) &&
-         (out_cfg.asymm_cipher == cfg.asymm_cipher) &&
-         (out_cfg.symm_cipher == cfg.symm_cipher) &&
-         (out_cfg.digest_algorithm == cfg.digest_algorithm));
-
-  assert(success == DECRYPT_SUCCESS);
-
-  assert(strncmp((const char *)plaintext_out.data,
-                 (const char *)plaintext_in.data, plaintext_in.data_len) == 0);
-  free((void *)plaintext_out.data);
-  if (plaintext_in.aad != NULL) {
-    assert(strncmp((const char *)plaintext_out.aad,
-                   (const char *)plaintext_in.aad, plaintext_in.aad_len) == 0);
-    free((void *)plaintext_out.aad);
-  }
-
-  peacemakr_key_free(key);
-}
-
 void test_wrong_key(symmetric_cipher cipher) {
 
   plaintext_t plaintext_in = {.data = (const unsigned char *)message,
@@ -268,8 +216,6 @@ int main() {
   if (!peacemakr_init()) {
     return 1;
   }
-
-  test_uninit_crash();
 
   for (int i = AES_128_GCM; i <= CHACHA20_POLY1305; ++i) {
     test_symmetric_algo(i);
