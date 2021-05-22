4a874504a68d5ea8afb5fc76baba7545c1103ed4
Jon McLachlan <jon.mclachlan@icloud.com>
2021-05-22 13:49:09 -0400

    fix openssl build

--- src/ffi/web/openssl/build-openssl.sh
+++ src/ffi/web/openssl/build-openssl.sh
@@ -9,14 +9,30 @@ mkdir -p ${INSTALL_DIR}/lib
 
 OPENSSL_VERSION=1.1.1k
 
-if [[ ! -d "openssl-${OPENSSL_VERSION}" ]]; then
+if [[ ! -f "openssl-${OPENSSL_VERSION}.tar.gz" ]]; then
     curl -O https://www.openssl.org/source/openssl-${OPENSSL_VERSION}.tar.gz
-    tar -xzf openssl-${OPENSSL_VERSION}.tar.gz
+fi
+
+if [[ ! -d "openssl-${OPENSSL_VERSION}" ]]; then
+  tar -xzf openssl-${OPENSSL_VERSION}.tar.gz
 fi
 
 pushd openssl-${OPENSSL_VERSION}
 
-CC=emcc CXX=em++ AR=emar RANLIB=emranlib ./Configure linux-generic64 no-asm no-ssl3 no-dso no-comp no-hw no-engine no-async -D__STDC_NO_ATOMICS__ --prefix=${INSTALL_DIR}
+CC=emcc CXX=em++ AR=emar RANLIB=emranlib ./Configure linux-generic32 \
+      no-asm \
+      no-err \
+      no-ssl2 \
+      no-ssl3 \
+      no-threads \
+      no-engine \
+      no-hw \
+      no-weak-ssl-ciphers \
+      no-dtls \
+      no-shared \
+      no-dso \
+      -DBN_DEBUG -D__STDC_NO_ATOMICS__=1 -DNO_FORK -DPEDANTIC -Oz \
+      --prefix=${INSTALL_DIR}
 
 sed 's/^CROSS_COMPILE.*$/CROSS_COMPILE=/g' Makefile
 
